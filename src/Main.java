23fe893b8dd58838dd07ec9388e484d816dbe491
Nikita Goryachev <ackerman_x@mail.ru>
2023-03-01 15:21:20 +0300

    fixed mistakes

--- src/Main.java
+++ src/Main.java
@@ -19,26 +19,26 @@ Set и Map.
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Map<String, Document> map = new HashMap<>();
+        Map<String, Document> documentMap = new HashMap<>();
         Set<String> contractSet = new HashSet<>();
         try (BufferedReader br = new BufferedReader(new FileReader("Document.txt"))) {
             String string;
             while ((string = br.readLine()) != null) {
-                String[] infPerson = string.split(",");
-                String number = infPerson[0].trim();
+                String[] infDocument = string.split(",");
+                String number = infDocument[0].trim();
                 if (!contractSet.contains(number)) {
                     contractSet.add(number);
                     Document doc = new Document();
-                    doc.setTitle(infPerson[1]);
-                    doc.setDate(infPerson[2]);
-                    doc.setAuthor(infPerson[3]);
-                    map.put(number, doc);
+                    doc.setTitle(infDocument[1]);
+                    doc.setDate(infDocument[2]);
+                    doc.setAuthor(infDocument[3]);
+                    documentMap.put(number, doc);
                 }
             }
             } catch(FileNotFoundException ex){
                 System.err.println("Не верный путь!");
             }
-        for (Map.Entry<String, Document> entry : map.entrySet()) {
+        for (Map.Entry<String, Document> entry : documentMap.entrySet()) {
             System.out.println("["+entry.getKey() + " -> " + entry.getValue()+"]");
         }
     }
